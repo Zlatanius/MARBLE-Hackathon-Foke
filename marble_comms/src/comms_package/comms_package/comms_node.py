0b5738ad07bfa4030dd21940fb8f62aed061c6ce
GoldenStrongy <zjakic2@etf.unsa.ba>
2023-08-26 22:41:13 +0200

    Added distance meassurment

--- marble_comms/src/comms_package/comms_package/comms_node.py
+++ marble_comms/src/comms_package/comms_package/comms_node.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+import numpy as np
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from sensor_msgs.msg import NavSatFix
+
+R = 6378  # Radius of the earth in metres
 
 
 class CommsNode(Node):
@@ -9,7 +13,52 @@ class CommsNode(Node):
         super().__init__('comms_node')
         # create a publisher that publishes an array of strings to the topic "comms_topic"
         self.publisher = self.create_publisher(String, 'comms_topic', 10)
-        self.create_timer(1.0, self.timer_callback)
+        self.self_position_subscriber = self.create_subscription(
+            NavSatFix, '/floater1/gnss', self.gnss_callback, 10)
+        self.nex_position_subscriber = self.create_subscription(
+            NavSatFix, '/floater2/gnss', self.gnss_callback, 10)
+        # self.create_timer(1.0, self.timer_callback)
+        self.latitude = 0
+        self.longitude = 0
+        self.altitude = 0
+        self.next_latitude = 0
+        self.next_longitude = 0
+        self.next_altitude = 0
+        self.self_x_coord = 0
+        self.self_y_coord = 0
+        self.next_x_coord = 0
+        self.next_y_coord = 0
+
+    def gnss_callback(self, msg):
+        caller_id = msg.header.frame_id
+        strings = caller_id.split("/", 2)
+        caller_id = strings[0]
+        if caller_id == "floater1":
+            self.latitude = msg.latitude
+            self.longitude = msg.longitude
+            self.altitude = msg.altitude
+
+            self.self_x_coord = np.sin(msg.longitude) * R
+            self.self_y_coord = np.sin(msg.latitude) * R
+
+            self.get_logger().info('My coordinattes: %f, %f, %f, %s' %
+                                   (msg.latitude, msg.longitude, msg.altitude, caller_id))
+            # self.get_logger().info('My coordinattes: %f, %f, %s' %
+            #                        (self.self_x_coord, self.self_y_coord, caller_id))
+        else:
+            self.next_latitude = msg.latitude
+            self.next_longitude = msg.longitude
+            self.next_altitude = msg.altitude
+
+            self.next_x_coord = np.sin(msg.longitude) * R
+            self.next_y_coord = np.sin(msg.latitude) * R
+
+            self.get_logger().info('Next floater coordinattes: %f, %f, %f, %s' %
+                                   (msg.latitude, msg.longitude, msg.altitude, caller_id))
+            # self.get_logger().info('My coordinattes: %f, %f, %s' %
+            #                        (self.next_x_coord, self.next_y_coord, caller_id))
+        self.get_logger().info("Distance between floaters: %f" % (2 * R * np.arcsin(np.sqrt(np.sin((self.next_latitude-self.latitude)/2)
+                                                                                            ** 2+np.cos(self.latitude)*np.cos(self.next_latitude)*np.sin((self.next_longitude-self.longitude)/2)**2))))
 
     def timer_callback(self):
         # Replace with your actual string array
